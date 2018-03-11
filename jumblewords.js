844db079b67161f7e36ed160eae3ce5ec8ecca0e
HarmanPC <harmansinghgwalior4@gmail.com>
2018-03-11 16:51:33 +0530

    Delete jumblewords.js

--- jumblewords.js
+++
@@ -1,7 +0,0 @@
-var original = words[Math.floor(Math.random() * words.length)];
-var word = original.split('').join(', ');
-
-var output = word;
-
-document.getElementById("jumble").innerHTML = original;
-document.getElementById("jumble2").innerHTML = output;
