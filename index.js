87d130b4881327fd4351ecd4e07904da773c4a23
Harman <harmansinghgwalior4@gmail.com>
2018-04-10 17:12:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,42 @@ function style(id) {
   if (!id) return;
   return onId(id).style;
 }
+style("title").color = "cyan";
+style("title").background = "#000000";
+style("h1").color = "red";
+style("button").background = "cyan";
+
+function shuffle(array) {
+	if (!Array.isArray(array)) return;
+ 	let currentIndex = array.length; 
+	let temporaryValue; 
+	let randomIndex;
+
+	while (0 !== currentIndex) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+ 		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+const original = words[Math.floor(Math.random() * words.length)];
+var word = original.split('');
+var output = shuffle(word).join(', ');
+
+write("jumble", output);
+var answer;
+function onLoad() {
+  answer = onId("answer");
+}
+function checkAnswer() {
+	if (answer.value.toLowerCase() === original.toLowerCase()) {
+		style("result").color = "green";
+		write("result", "You win!");
+	} else {
+    		style("result").color = "red";
+		write("result", "Wrong.");
+	}
+}
