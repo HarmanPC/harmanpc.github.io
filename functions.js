d31c6dec53976da9b754ccac595a222440cb9627
Harman <harmansinghgwalior4@gmail.com>
2018-04-10 16:39:25 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,35 +1,36 @@
-	function shuffle(array) {
- 		var currentIndex = array.length; 
-		var temporaryValue; 
-		var randomIndex;
+function shuffle(array) {
+	if (!Array.isArray(array)) return;
+ 	let currentIndex = array.length; 
+	let temporaryValue; 
+	let randomIndex;
 
-		while (0 !== currentIndex) {
+	while (0 !== currentIndex) {
                 randomIndex = Math.floor(Math.random() * currentIndex);
                 currentIndex -= 1;
 
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
-            }
-            return array;
-        }
-	var original = words[Math.floor(Math.random() * words.length)];
-	var word = original.split('');
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+ 		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+var original = words[Math.floor(Math.random() * words.length)];
+var word = original.split('');
 
-	var output = shuffle(word).join(', ');
+var output = shuffle(word).join(', ');
 
-	document.getElementById("jumble").innerHTML = output;
-  var answer;
-  function onLoad() {
+write("jumble", output);
+var answer;
+function onLoad() {
   answer = document.getElementById("answer");
   }
   function checkAnswer() {
-	  var result = document.getElementById("ok");
+	var result = id("result");
 	if (answer.value.toLowerCase() === original.toLowerCase()) {
 		result.style.color = "green";
-		result.innerHTML = "You win!";
+		write("result", "You win!");
 	} else {
     		result.style.color = "red";
-		result.innerHTML = "Wrong.";
-    }
-  }
+		write("result", "Wrong.");
+	}
+}
