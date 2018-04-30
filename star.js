2c8b49e81d9b74904d378dc6bbb712d104bf3a6e
Harman <harmansinghgwalior4@gmail.com>
2018-04-30 19:03:56 +0530

    Create star.js

---
+++ star.js
@@ -0,0 +1,23 @@
+class Star {
+    constructor(x,  y,  radius1,  radius2,  points) {
+        this.x = x;
+        this.y = y;
+        this.radius1 = radius1;
+        this.radius2 = radius2;
+        this.points = points;
+    }
+    show() {
+        var angle = TWO_PI / this.points;
+        var halfAngle = angle/2.0;
+        beginShape();
+        for (var a = 0; a < TWO_PI; a += angle) {
+            var sx = this.x + cos(a) * this.radius2;
+            var sy = this.y + sin(a) * this.radius2;
+            vertex(sx, sy);
+            sx = this.x + cos(a+halfAngle) * this.radius1;
+            sy = this.y + sin(a+halfAngle) * this.radius1;
+            vertex(sx, sy);
+        }
+        endShape(CLOSE);
+    }
+}
